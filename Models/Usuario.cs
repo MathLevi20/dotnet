b3cfa30db72ef4257adc75228dbaec99b4d58dcd
Levi <matheuslevi2012@gmail.com>
2022-10-18 01:20:02 -0300

    feat: Start Project

---
+++ Models/Usuario.cs
@@ -0,0 +1,11 @@
+
+namespace dotnet.Models
+{
+public class Usuario
+{
+public int Id { get; set; }
+public string Nome { get; set; }
+public string Login { get; set; }
+public string Password { get; set; }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using dotnet.Models;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddEntityFrameworkNpgsql()
+    .AddDbContext<Context>(options =>
+    options.UseNpgsql("Host=localhost;Port=5432;Pooling=true;Database=levi_linux;User Id=levi_linux;Password=12345"));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
